0926793ca79c8aace45c6893a7969c037a29c4e0
janekigenyi <janekigenyi@gmail.com>
2022-09-23 09:46:17 +0300

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,11 @@
 import React from "react";
-
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <links linkedin={props.links.linkedin} github={props.links.github}/>
     </div>
   );
 }
